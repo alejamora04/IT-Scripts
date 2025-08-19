9b8f3acd332e36a3724bbe23d66853168cf07624
Alejandro Morales <alejandro.morales04@gmail.com>
2025-08-18 22:12:20 -0500

    refactored and prototyping formatted output.

--- Drive_Script/Drive_Script.ps1
+++ Drive_Script/Drive_Script.ps1
@@ -1,14 +1,23 @@
 <#
-Get attached/detected drive information export to file labeled Drive_Info.txt in child directory Output
+Gathers useful information about attached/detected drive information mounted on this workstation. 
+Exports to file labeled Drive_Info.txt in child directory Output
 
-Gathers information about all drives attached and mounted on this workstation.
-List useful information such as usage, availability, file system type, attached/networked, share path permissions.
-Output data to a file labeled Drive_Info.txt.
+List useful information such as 
+- Usage 
+- Availability
+- Fiile system type
+- Attached or Networked 
+- Share Path 
+- Permissions.
+
+Outputs: data to a file labeled Drive_Info.txt.
 
 #>
 
 # Define Output Dir: Export results to a text file labeled Drive_Info.txt in a child directory labeled Output.
 $outputFilePath = Join-Path -Path $PSScriptRoot -ChildPath ".\Output\Drive_Info.txt"
+# TODO: Create .json file to export as well to maintain verbose serialized raw information. 
+$outputCSV = Join-Path -Path $PSScriptRoot -ChildPath ".\Output\Drive_Info.csv"
 
 # Gather information about all drives attached to this computer and mounted.
 # Create variables to contain drive Data for physical disk[drive Info], mounted drives [volInfo], networked shares [netShareInfo] 
@@ -16,16 +25,17 @@ $driveInfo = Get-Disk
 $volInfo = Get-Volume
 $netShareInfo = Get-SmbShare | Select-Object *
 
-# Header with Timestamp for script execution.
+# Header to add Timestamp to log for script execution.
 "Drive Report generated on $(Get-Date)" | Out-File -FilePath $outputFilePath
 
 # Write variables to output file labeled Drive_Info.txt
 "----- Physical Disk Info -----" | Out-File -FilePath $outputFilePath -Append
-$driveInfo | Out-File -FilePath $outputFilePath -Append
+$driveInfo | Format-Table -AutoSize | Out-File -FilePath $outputFilePath -Append
+$driveInfo | Export-Csv -Path $outputCSV
 "----- Volume Info -----" | Out-File -FilePath $outputFilePath -Append
 $volInfo | Out-File -FilePath $outputFilePath -Append
 "----- Network Shares -----" | Out-File -FilePath $outputFilePath -Append
 $netShareInfo | Out-File -FilePath $outputFilePath -Append
 
 # Wait for input to close the console window
-Read-Host -Prompt "[+] The script has finished executing. Press Enter to Exit"
+Read-Host -Prompt "[+] The script has finished executing files located in $outputFilePath . Press Enter to Exit"
